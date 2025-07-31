6da7ef28da40279bd6b1ef1a37d73dd0e17d3cd6
Rishikesh <rishiarote2020@gmail.com>
2025-07-31 16:13:06 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Replace with your actual MySQL credentials
+#change this to my pc 
 DATABASE_URL = "mysql+pymysql://root:rishikesh@localhost/expense_tracker_db"
 # DATABASE_URL = "mysql+pymysql://root:rishikesh@host.docker.internal:3306/expense_tracker_db"
 engine = create_engine(DATABASE_URL)
